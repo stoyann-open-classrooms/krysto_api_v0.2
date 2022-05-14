08ee5a024eebd1fa9f402c462295c75a1c186770
Stoyann <stoyann.openclassrooms@gmail.com>
2022-05-14 17:16:42 +1100

    création du server http

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+//imports
+const express = require("express");
+
+//instantiate server
+const server = express();
+
+//testing api
+server.get(`/`, (req, res) => res.send(`🌏🌏 Im online ! 🌏🌏   well done 👍`));
+server.get("*", (req, res) =>
+  res.status(501).send(`What the hell are you doing`)
+);
+// Launch server
+
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(
+    `✅ This server running on port ${process.env.SERVER_PORT}. Have fun 👍👍`
+  );
+});
