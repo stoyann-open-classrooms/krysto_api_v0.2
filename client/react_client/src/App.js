3f8e846e2626f40b15e4a64c60dd483978e8d297
Stoyann <stoyann.openclassrooms@gmail.com>
2022-05-15 14:56:16 +1100

    mise en place du client

--- client/react_client/src/App.js
+++ client/react_client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import logoKrysto from "./logo_krysto.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logoKrysto} className="krysto_logo" alt="" />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Le site de Krysto est en cours de construction...</p>
       </header>
+      <main></main>
     </div>
   );
 }
